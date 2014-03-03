a25f4efcbc2bcc0693167cfb5f91fb5e98d2740c
roxannemoslehi <rmoslehi@berkeley.edu>
2014-03-03 15:52:45 -0800

    draft2

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ $(document).ready(function(){
  function Dog() {
  }
  Dog.prototype.bark = function() {
- 	console.log('woof');
+     console.log('woof');
  }
  function ShibaInu() {
  }
  ShibaInu.prototype = new Dog();
  ShibaInu.prototype.constructor = ShibaInu;
  ShibaInu.prototype.bark = function() {
- 	console.log('much wow very javascript');
+     console.log('much wow very javascript');
  }
 
  //QUESTION 3
 $('.secondbutton').click(function() {
- $(this).css('background', '#00CED1');
- $(this).css('border-color', '#00CED1');
- $(this).parent().find('.secondheading').html('horse');
+    $(this).css('background', '#00CED1');
+    $(this).css('border-color', '#00CED1');
+    $(this).parent().find('.secondheading').html('horse');
  });
 
  //QUESTION 4
 $('#task4').click(function() {
- $(this).parent().find('#hipcat').css('-webkit-filter', 'blur(3px) sepia(.4)');
+    $(this).parent().find('#hipcat').css('-webkit-filter', 'blur(3px) sepia(.4)');
  });
 
  //QUESTION 5
 $('#task5').click(function() {
-	$('.spincar').css('-webkit-animation-name', 'drive');
+    $('.spincar').css('-webkit-animation-name', 'drive');
 	$('.spincar').css('-webkit-animation-play-state', 'running');
 	$('.spincar .wheel').css('-webkit-animation-name', 'spin');
 	$('.spincar .wheel').css('-webkit-animation-play-state', 'running');
@@ -37,7 +37,7 @@ $('#task5').click(function() {
 
  //QUESTION 6
  $('.nyancat').click(function() {
-  $(this).animate({left:'85%'}, 'slow');
-  $(this).parent().find('.rainbows').animate({width: '78%'}, 'slow');
+     $(this).animate({left:'85%'}, 'slow');
+     $(this).parent().find('.rainbows').animate({width: '78%'}, 'slow');
  });
 });
